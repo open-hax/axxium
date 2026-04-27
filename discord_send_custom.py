0484b3065ab3176ffacfb3093f998918f8b12604
Error <foamy125@gmail.com>
2026-04-27 00:17:26 -0500

    hacks

---
+++ discord_send_custom.py
@@ -0,0 +1,40 @@
+import discord
+import asyncio
+import os
+import sys
+
+TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+
+async def main():
+    if len(sys.argv) < 3:
+        print('Usage: python3 discord_send_custom.py <channel_id> "<message>"')
+        return
+
+    channel_id = int(sys.argv[1])
+    message_text = sys.argv[2]
+    
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+    
+    ready_future = asyncio.get_event_loop().create_future()
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
+        channel = client.get_channel(channel_id)
+        if channel:
+            await channel.send(message_text)
+            print(f'Sent: {message_text}')
+        else:
+            print('Channel not found')
+        ready_future.set_result(True)
+
+    try:
+        await client.start(TOKEN)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
